c6834885e191ce79c26e571e4f94d791530bd832
Cristhian BR <cristhianalexand@gmail.com>
2023-03-16 19:12:47 -0500

    Actualización global

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,8 +1,9 @@
 import "./HeaderComponent.css";
 import myIcon from './../../sources/img/logo/logo.ico'
+import { useState } from "react";
 
-const HeaderComponent = ({optionAct, locationSection, sections}) => {
-    
+const HeaderComponent = ({ optionAct, locationSection, sections }) => {
+    const [isActiveMore, setIsActiveMore] = useState(false);
 
     const styleOption = [{
         color: "#aba6a6",
@@ -22,28 +23,36 @@ const HeaderComponent = ({optionAct, locationSection, sections}) => {
     //         }
     //     }))
     // });
-    
-    
+
+    const activeFunction= ()=>{
+        if(!isActiveMore)
+            setIsActiveMore(true)
+        else
+            setIsActiveMore(false)
+    }
 
     return (
         <header className="HeaderComponent-class">
             <nav>
 
                 <img src={myIcon} alt="Lol" />
-
-                <div>
+                <svg onClick={()=>activeFunction()} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+                </svg>
+                <div className={(isActiveMore)?"navDivs__active":""}>
                     {sections.map((section, num) => {
-                        
-                        return(<>
-                            <a onClick={()=>locationSection(num)}
-                                style={(optionAct===num)?styleOption[1]:styleOption[0]}>{section}</a>
+
+                        return (<>
+                            <a onClick={() => locationSection(num)}
+                                style={(optionAct === num) ? styleOption[1] : styleOption[0]}>{section}</a>
                             {(num + 1 < sections.length) ? (<span >|</span>) : null}
-                        </>)}
+                        </>)
+                    }
                     )}
                 </div>
             </nav>
         </header>
     )
-    
+
 }
 export default HeaderComponent;
